ad33897da40e532fc8c64169389539fb111314ed
eladshachar <elad.shachar@gmail.com>
2022-10-23 12:00:36 +0300

    basic backend

---
+++ backend/allergens.sql
@@ -0,0 +1,12 @@
+-- CREATE DATABASE recipes_app;
+
+USE recipes_app;
+
+-- CREATE TABLE allergens_ingredients(
+--     name VARCHAR(255),
+--     ingredient VARCHAR(255)
+-- );
+
+-- INSERT INTO allergens_ingredients VALUES("gluten", "Beer")
+
+-- SELECT ingredient FROM allergens_ingredients WHERE name = 'dairy'
